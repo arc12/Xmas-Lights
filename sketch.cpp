54592aeb818cc498739879245636bb570abc2f5f
Adam Cooper <arc@hilltop-cottage.info>
2014-12-27 21:52:28 +0000

    updates - mostly complete and tested except for live programming

--- sketch.cpp
+++ sketch.cpp
@@ -39,6 +39,9 @@ void programAll(uint8_t shape, uint8_t rateSrc, uint8_t scaleSrc, uint8_t tgSrc)
 		void loadProgram(uint8_t programNumber);
 		uint8_t decodeNumIR(unsigned long irCode);
 
+uint16_t getSrcVal(uint8_t src);
+void stepButton(uint8_t src);
+
 
 /*
  * Place a direct direct copy of sketch (*.ino or *.pde) beneath this comment block.... except that you:
@@ -71,7 +74,7 @@ void programAll(uint8_t shape, uint8_t rateSrc, uint8_t scaleSrc, uint8_t tgSrc)
 // array to hold source values, e.g. ADC readings, and the indeces of each source.
 //Most are re-populated periodically (but not necessarily on each loop) or on an event
 // Va;lues are always in the range 0-1023
-uint16_t srcVals[16+NUM_LEDS]={0,1023,512};//only set the values for ON, OFF, and HALF which are constants
+uint16_t srcVals[16+NUM_LEDS]={0,1023,512,0,0,0,0,0,0,0,512,512,512,512};
 #define SRC_OFF 0 //permanently OFF, i.e. value = 0
 #define SRC_ON 1 //always ON, i.e. value =1023
 #define SRC_HALF 2 //always value = 512
@@ -79,9 +82,10 @@ uint16_t srcVals[16+NUM_LEDS]={0,1023,512};//only set the values for ON, OFF, an
 #define SRC_LEV1 4 //VR1 (ADC1)
 #define SRC_LEV2 5 //VR2 or thermister (ADC2)
 #define SRC_LEV3 6 //VR3 or LDR (ADC3)
-#define SRC_SW1 7 //switch 1 | IR controlled boolean #1
-#define SRC_SW2 8 //switch 2 | IR controlled boolean #2
-#define SRC_SW3 9 //switch 3 | IR controlled boolean #3
+//the next six should be continguous and in the same order
+#define SRC_STEP1 7 //button 1 | IR controlled button #1 ... buttons step through values 0, 255, 510, 765, 1020
+#define SRC_STEP2 8 //button 2 | IR controlled button #2
+#define SRC_STEP3 9 //button 3 | IR controlled button #3
 #define SRC_IR_INT1 0xA //IR controlled integer value #1
 #define SRC_IR_INT2 0xB //IR controlled integer value #2
 #define SRC_IR_INT3 0xC //IR controlled integer value #3
@@ -90,6 +94,7 @@ uint16_t srcVals[16+NUM_LEDS]={0,1023,512};//only set the values for ON, OFF, an
 #define SRC_RND_10S 0xF //Random number in range 0-1023, changing every 10 seconds
 #define SRC_TG_MASK_BASE 0x10 //srcVals index at which the trigger/gate mask values reside (there are NUM_LEDS of them).
 //NB TG_MASK is, in principle, available for rate and scale patches, but is NOT intended for use that way
+#define SRC_CONST 0x80 //effectively a bit indicator that the (lowest 7 bits <<3) is a "constant value source". #defined here mostly as documentation
 
 // array to hold the patches - i.e. the mapping from the values in srcVals to parameters passed to the ShapedBrightnessController
 // each LED has 3 parameters, with the precise details of how they affect the brightness over time being determined by the pattern
@@ -108,6 +113,7 @@ decode_results results;
 //next 4 to store received commands
 unsigned long irLast;//used to read in the last received value
 unsigned long irCommand;//used to store the active command (i.e. the command that started a sequence of key presses)
+uint8_t irSrc = SRC_STEP1;//which src is currently affected by PLUS/MINUS inputs (in run mode)
 uint8_t irTens;//for numeric inputs, the decoded value in the tens column
 uint8_t irUnits;//ditto units
 // codes for remote control buttons. This is based on the ubiquitous "car MP3" sender, but the button names
@@ -138,6 +144,12 @@ uint8_t irUnits;//ditto units
 uint8_t programCount;//number of programs in EEPROM
 uint8_t currentProgram;//the index of the currently-loaded program.
 
+//Program cycling. The program changes at intervals, indicated by pcRateSrc
+//cycling uses currentProgram, which is fixed in the range 1..programCount
+uint8_t pcRateSrc;//the contents of EEPROM address 0x02, which is a value from SRC_*
+uint16_t pcCounter=0;////gets getSrcVal(pcRateSrc) added each tick. When exceeds 2048 the mask changes by one step and counter resets to 0
+boolean pcActive = true;//cycling active
+
 //object for time-varying LED controllers
 ShapedBrightnessController sbc = ShapedBrightnessController(NUM_LEDS);
 
@@ -146,7 +158,8 @@ unsigned long lastRandChange1;
 unsigned long lastRandChange10;
 
 //A Low Frequency Osc (LFO) - triangle form - can be used as a SRC in a patch, but also has its freq controlled by SRC value via the special LFO patch
-uint16_t lfoRate=1;//rate of change.
+uint8_t lfoRateSrc;
+uint16_t lfoCounter=0;
 
 //The "trigger/gate mask" provides an on/off time-varying pattern (e.g. moving dot, bar, etc) that feeds SRC_TG_MASK_BASE+led
 //and can be used as a patch source. Its rate can be set by other SRC inputs.
@@ -154,7 +167,7 @@ uint16_t tgMask=1;//stores the bit mask
 uint8_t runLength = NUM_LEDS;
 uint16_t tgMaskMask=(1<<NUM_LEDS)-1; //tgMask is forced to = tgMask & tgMaskMask in some cases
 uint16_t tgmCounter=0;//gets tgmRate added each tick. When exceeds 2048 the mask changes by one step and counter resets to 0
-uint16_t tgmRate=1;//rate of change.
+uint16_t tgmRateSrc=1;//source for rate of change. Any valid SRC including CONST
 uint8_t tgmPattern=0;// stores the active change pattern - see the following #defines. //NB: top nibble assumed to hold modifiers, low nibble to code for basic pattern
 bool tgmAB=true;//some patterns alternate. This keeps whether A or B motion is in force.
 //mask change patterns
@@ -165,6 +178,7 @@ bool tgmAB=true;//some patterns alternate. This keeps whether A or B motion is i
 #define TGM_DOUBLE 4// like SINGLE 2 bits on
 #define TGM_TRIPLIFY 128 //add this to treat the change pattern as an RGB pattern and replicate across all RGB triples
 
+boolean lastButton[3];
 unsigned long lastTickMillis;
 
 void setup(){
@@ -192,11 +206,13 @@ void setup(){
 	//first byte is number of LEDs in the programs. Must match NUM_LEDS otherwise there are 0 programs available.
 	if(EEPROM.read(0)==NUM_LEDS){
 		programCount = EEPROM.read(1);
+		pcRateSrc = EEPROM.read(2);
 	}else{
 		//correct the error, and assert 0 programs
 		EEPROM.write(0,NUM_LEDS);
 		EEPROM.write(1,0);
 		programCount =0;
+		pcActive=false;
 	}
 	
 	//Load program 1 if available, otherwise a default program
@@ -259,15 +275,28 @@ void loop(){
 	uint16_t rate;
 	if((millis()-lastTickMillis) > 62){
 		
+		//if not waiting for program load commands, and program cycling is active then process the program cycling rules
+		if(pcActive && (irCommand == 0)){
+			pcCounter+=(getSrcVal(pcRateSrc)>>5);
+			if(pcCounter>=2048){
+				currentProgram++;
+				if(currentProgram>programCount){
+					currentProgram = 1;
+				}
+				loadProgram(currentProgram);
+				pcCounter = 0;
+			}
+		}
+		
 		//check IR receiver.
 		if (irrecv.decode(&results)) {
 			irLast = results.value;
-			#ifdef DEBUG
-			Serial.println(irLast, HEX);//value is an unsigned long
-			#endif
 			irrecv.resume(); // Receive the next value
 			//only proceed if it was not a "repeat last value" code
 			if(irLast!=0xFFFFFFFF){
+				#ifdef DEBUG
+				Serial.println(irLast, HEX);//value is an unsigned long
+				#endif
 				boolean loadOK=false;//gets set to true if prog is to be loaded (several routes to this situation)
 				//if current command is PROG then need up to two numbers terminated by OK (or cancel to finish)
 				if(irCommand == IR_PROG){
@@ -278,19 +307,22 @@ void loop(){
 							currentProgram=progNum;
 						}
 						irCommand=0;//also cancel the active command
-					}else if(irLast == IR_PLUS){
+					}else if(irLast == IR_NEXT){
 						currentProgram++;
 						if(currentProgram>programCount){
 							currentProgram=1;
 						}
 						loadOK = true;
-					}else if (irLast == IR_MINUS){
+					}else if (irLast == IR_PREV){
 						currentProgram--;
 						if(currentProgram==0){
 							currentProgram=programCount;
 						}				
 						loadOK = true;		
 					}else if(irLast == IR_CANCEL){
+						irCommand = 0;
+					}else if(irLast == IR_PLAY){
+						pcActive = !pcActive;
 						irCommand = 0;
 					}else{
 						uint8_t irNumber = decodeNumIR(irLast);//gets the decimal number for the last key. returns 255 if not a number
@@ -314,10 +346,48 @@ void loop(){
 				}
 				
 				//does the last value start a PROG sequence? Only do it if there are programs.
-				if(irLast == IR_PROG && (programCount>0)){
-					irCommand = IR_PROG;
-					irTens = 0;
-					irUnits = 0;
+				if(irLast == IR_PROG){
+					if(programCount>0){
+						irCommand = IR_PROG;
+						irTens = 0;
+						irUnits = 0;
+					}
+				}else if(irLast == IR_CH_MINUS){
+					irSrc--;
+					if(irSrc<SRC_STEP1)irSrc = SRC_IR_INT3;
+				}else if(irLast == IR_CH_PLUS){
+					irSrc++;
+					if(irSrc>SRC_IR_INT3)irSrc = SRC_STEP1;
+				} else if((irLast == IR_MINUS) || (irLast == IR_PLUS)){
+					//change STEP or IR_INT srcVals
+					int step=(irSrc<SRC_IR_INT1)?255:32;
+					int val = (int)srcVals[irSrc];
+					if(irLast == IR_MINUS){
+						val-=step;
+						if(val<0) val = 0;
+					}else{
+						val+=step;
+						if(val>=1024) val = 1023;
+					}
+					srcVals[irSrc] = (uint16_t)val;
+						//flash activity pin to ack
+						digitalWrite(PIN_ACT, HIGH);
+						delay(100);
+						digitalWrite(PIN_ACT, LOW);
+					#ifdef DEBUG
+					Serial.println(val);
+					#endif
+				}
+				
+				//some flashing when IR srcValue pointer changes (this does not count as a command sequence... see below)
+				if((irLast == IR_CH_MINUS) || (irLast == IR_CH_PLUS)){
+					int flashLength = (irSrc>SRC_STEP3?250:100);
+					for(uint8_t ii=0;ii<1+(irSrc - SRC_STEP1)%3;ii++){
+						digitalWrite(PIN_ACT, HIGH);
+						delay(flashLength);			
+						digitalWrite(PIN_ACT, LOW);
+						delay(flashLength);
+					}
 				}
 				
 				//turn the activity LED on while a sequence is expected and off when not
@@ -330,8 +400,15 @@ void loop(){
 		}
 
 		lastTickMillis=millis();
+		
 		//update the LFO value
-		//-- to do
+		lfoCounter+=(getSrcVal(lfoRateSrc)>>4);
+		if(lfoCounter>=2048) lfoCounter -=2048;
+		if(lfoCounter<1024){
+			srcVals[SRC_LFO]=lfoCounter;
+		}else{
+			srcVals[SRC_LFO]=2048 - lfoCounter;
+		}
 		
 		//update the trigger/gate mask
 		if(!(tgmPattern==TGM_DISABLED)){
@@ -340,18 +417,16 @@ void loop(){
 		
 		//use the patches to set the LED change rate, brightness scale, or trigger/gate input
 		for(uint8_t led = 0; led< NUM_LEDS; led++){
-			rate = srcVals[patches[led][PAR_RATE]];
-			if(rateFactor>0){//rateFactor scales the rate by factors of two
-				rate = rate << rateFactor[led];
-			}else if(rateFactor<0){
-				rate = rate >> (-rateFactor[led]);
+			rate = getSrcVal(patches[led][PAR_RATE]);
+			signed char rf=rateFactor[led];
+			if(rf>0){//rateFactor scales the rate by factors of two
+				rate = rate << rf;
+			}else if(rf<0){
+				rate = rate >> -rf;
 			}
-			//Serial.print(led);
-			//Serial.print(" ");
-			//Serial.println(rate);
 			sbc.setRate(led, rate);
-			sbc.setScale(led, srcVals[patches[led][PAR_SCALE]]);
-			sbc.setTriggerIP(led, srcVals[patches[led][PAR_TG_IP]]);
+			sbc.setScale(led, getSrcVal(patches[led][PAR_SCALE]));
+			sbc.setTriggerIP(led, getSrcVal(patches[led][PAR_TG_IP]));
 		}
 		sbc.tick();
 	}
@@ -362,7 +437,7 @@ void loop(){
 void updateTGM(){
 	uint8_t tgmPattern2 = tgmPattern & 0xF;
 	//gets tgmRate added each tick. When exceeds 2048 the mask changes by one step and counter resets to 0
-	 tgmCounter+=tgmRate;
+	 tgmCounter+=(getSrcVal(tgmRateSrc)>>2);
 	 if(tgmCounter>=2048){
 		 //change mask
 		 switch (tgmPattern2){
@@ -409,24 +484,11 @@ void updateTGM(){
 				}
 				break;
 		 }
-		 ////check if the pattern needs a reset or an AB bounce
-		 //if( (tgMask>=tgMaskOver) || ((tgmPattern&TGM_TRIPLIFY) && (tgMask>=8) ) ){
-			 //if( (tgmPattern2==TGM_SINGLE) || (tgmPattern2==TGM_DOUBLE) ){
-				//tgMask = 1;			 
-			 //}else{
-				 //if(tgmAB){
-					 //tgMask = 1;
-				 //}
-				 //tgmAB = !tgmAB;
-			 //}
-		 //}
-		 //if(tgMask==1){//bottom bounce
-			 //tgmAB = !tgmAB;
-		 //}
 		 
 		 //transfer mask to srcVals
-		 for(uint8_t i=0; i<runLength; i++){
-			srcVals[SRC_TG_MASK_BASE+i]=(tgMask&(1<<i))?1023:0;		 
+		 for(uint8_t i=0; i<NUM_LEDS; i++){
+			 uint8_t ii = i%runLength;
+			srcVals[SRC_TG_MASK_BASE+i]=(tgMask&(1<<ii))?1023:0;		 
 		 }
 		 //reset counter
 		 tgmCounter = 0;
@@ -441,9 +503,21 @@ void readSourceValues(){
 	srcVals[SRC_LEV1] = analogRead(PIN_LEV1);
 	srcVals[SRC_LEV2] = analogRead(PIN_LEV2);
 	srcVals[SRC_LEV3] = analogRead(PIN_LEV3);
-	srcVals[SRC_SW1] = digitalRead(PIN_SW1)?1023:0;
-	srcVals[SRC_SW2] = digitalRead(PIN_SW2)?1023:0;
-	srcVals[SRC_SW3] = digitalRead(PIN_SW3)?1023:0;
+	boolean b = digitalRead(PIN_SW1);
+	if(b^lastButton[0]){
+		 lastButton[0] = b;
+		 stepButton(SRC_STEP1);
+	}
+	b = digitalRead(PIN_SW2);
+	if(b^lastButton[1]){
+		lastButton[1] = b;
+		stepButton(SRC_STEP2);
+	}
+	b = digitalRead(PIN_SW3);
+	if(b^lastButton[2]){
+		lastButton[2] = b;
+		stepButton(SRC_STEP3);
+	}
 	
 	//random changes at 1 and 10 seconds intervals
 	unsigned long t = millis();
@@ -455,6 +529,23 @@ void readSourceValues(){
 		srcVals[SRC_RND_10S] = random(1024);
 		lastRandChange10=t;
 	}	
+}
+
+void stepButton(uint8_t src){
+	uint16_t oldVal=srcVals[src];
+	oldVal+=255;
+	if(oldVal>1024) oldVal = 0;
+	srcVals[src] = oldVal;
+}
+
+//gets the actual value that currently pertains for a given source.
+//This copes with the CONST pseudo-sources, which simple look-up in srcVals could not ...
+uint16_t getSrcVal(uint8_t src){
+	if(src&SRC_CONST){
+		return (src&(~SRC_CONST)) <<3;
+	}else{
+		return srcVals[src];
+	}
 }
 
 // - ----------- programming helpers ------------
@@ -536,9 +627,9 @@ void loadProgram(uint8_t programNumber){
 	
 	//read in LFO and Trigger/gate mask settings
 	EEPROMUtils::loadBytes(&eAddr, buff, 4);
-	lfoRate = (uint16_t)buff[0];
+	lfoRateSrc = (uint16_t)buff[0];
 	EEPROMUtils::loadBytes(&eAddr, buff, 4);
-	tgmRate = (uint16_t)buff[0];
+	tgmRateSrc = (uint16_t)buff[0];
 	tgmPattern = buff[1];
 	tgMask = 1;
 	runLength = (tgmPattern&TGM_TRIPLIFY)?3:NUM_LEDS;
